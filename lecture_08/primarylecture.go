14e959e5cf21019e517406308fcb291fef1e6345
gdimran98 <gdimran@gmail.com>
2021-02-07 21:22:42 +0600

    Go learning

---
+++ lecture_08/primarylecture.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+func main(){
+	/*
+	var name string = "IMRAN"
+	var userName, city string = "Hossain", "DHAKA"
+	fmt.Println(name,userName,city)
+	*/
+
+
+	var c rune = 'A'
+var age int =10
+var result float32 = 50.55
+
+fmt.Println(c,age,result)
+}
