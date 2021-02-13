187139b8a48913b783d6b1a3a4cd3d677396c364
gdimran98 <gdimran@gmail.com>
2021-02-13 17:13:14 +0600

    Learn sturuct and scope variable and also define and run different go file

---
+++ lecture_11/custom.go
@@ -0,0 +1,36 @@
+package main
+
+type weight float32
+
+var name string ="Imran"
+
+
+type Book struct{
+	Title string
+	Author string
+	ISBN	string
+	Price float32
+	Pages int
+}
+
+var b1 Book
+/*b1.Title ="An Introduction to programming Go"
+b1.Author="CALEB DOXY"
+b1.ISBN="978-1478355823"
+b1.Price=10.50
+b1.Pages=165*/
+
+//fmt.Println(b1)
+
+/*
+car := struct{
+	make string
+	model string
+	mileage int
+}{
+	make: "Ford",
+	model: "Taurus",
+	mileage: 20000,
+}*/
+
+//fmt.Println(car)
---
+++ lecture_11/main.go
@@ -0,0 +1,21 @@
+
+package main
+
+import "fmt"
+
+var w1 weight 
+func main(){
+
+
+w1=30.50
+fmt.Println(w1,name)
+b1.Title ="An Introduction to programming Go"
+b1.Author="CALEB DOXY"
+b1.ISBN="978-1478355823"
+b1.Price=10.50
+b1.Pages=165
+fmt.Println(b1)
+//fmt.Println(car)
+}
+
+
